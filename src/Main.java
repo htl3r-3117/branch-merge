8f66eb06c1386fbf43adf78a7ced8ee42c9a5a91
htl3r-3117 <195273376+htl3r-3117@users.noreply.github.com>
2025-05-22 12:10:05 +0200

    2. Commit am Branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello("World");
     }
 
     public static void hello(String print) {
